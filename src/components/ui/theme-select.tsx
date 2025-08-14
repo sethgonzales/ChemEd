1055d4e5399f03675d20e2f78d27d9b1f2d4e0a6
sethgonzales <sethgonzales157@gmail.com>
2025-08-14 08:18:54 -0700

    add custom labels to theme select

--- src/components/ui/theme-select.tsx
+++ src/components/ui/theme-select.tsx
@@ -1,19 +1,17 @@
-// components/ThemeSelect.tsx
 import { THEME } from '~/styles/colors';
 import { useTheme } from '~/styles/theme-provider';
 
 export function ThemeSelect() {
   const { themeName, setThemeName } = useTheme();
-
   return (
     <select
       className="border rounded px-2 py-1"
       value={themeName}
       onChange={(e) => setThemeName(e.target.value as keyof typeof THEME)}
     >
-      {Object.keys(THEME).map((key) => (
+      {Object.entries(THEME).map(([key, value]) => (
         <option key={key} value={key}>
-          {key}
+          {value.label}
         </option>
       ))}
     </select>
--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -1,29 +1,34 @@
 export const THEME = {
   purple: {
+    label: 'Default',
     primary: '#8186e4',
     primaryText: '#ffffff',
     secondary: '#fab87e',
     secondaryText: '#000000',
   },
   black: {
+    label: 'Cougar black',
     primary: '#000000',
     primaryText: '#ffffff',
     secondary: '#dc2626',
     secondaryText: '#ffffff',
   },
   teal: {
+    label: 'Jaguar teal',
     primary: '#14b8a6',
     primaryText: '#ffffff',
     secondary: '#000000',
     secondaryText: '#ffffff',
   },
   red: {
+    label: 'Raider red',
     primary: '#dc2626',
     primaryText: '#ffffff',
     secondary: '#facc15',
     secondaryText: '#000000',
   },
   'burnt-orange': {
+    label: 'Warrior Orange',
     primary: '#c2410c',
     primaryText: '#ffffff',
     secondary: '#ffffff',
